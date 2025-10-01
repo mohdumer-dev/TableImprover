515af3bdeda13aa2a75163b6b4748aa8d2d2219f
CodeWithWasifTheGreat <your-email@example.com>
2025-10-01 22:24:54 +0530

    commited

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,6 @@ const  corsOptions = {
     'http://localhost:5173',
     'https://frontendtable.vercel.app',
   ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200 
 
 }
