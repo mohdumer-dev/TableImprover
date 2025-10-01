490ce85d703d595e32217cc2e13251e2575d17a1
CodeWithWasifTheGreat <your-email@example.com>
2025-10-01 15:07:09 +0530

    dasd

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,17 @@ import cors from 'cors'
 
 
 const  corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://frontendtable.vercel.app',
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200 
+  // origin: [
+  //   'http://localhost:5173',
+  //   'https://frontendtable.vercel.app',
+  // ],
+  // credentials: true,
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+  // optionsSuccessStatus: 200 
+
 }
-app.use(cors(corsOptions))
+app.use(cors())
 
 import SessionRouter from './routes/Session.js';
 
